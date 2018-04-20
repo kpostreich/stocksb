ea5f5f028f51345517c1e474baf1e5758a7a77c3
kpostreich <kevinlp@us.ibm.com>
2018-04-20 10:10:13 -0700

    fix Example.java to work with PathPaarameter

--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -7,18 +7,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.ArrayList;
 import java.util.List;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
 
 @RestController
 public class Example {
 
+	 
+  private static final double TEST_PRICE = 123.45;
+  private String stock = "IBM";
+  
 
-  @RequestMapping("v1/")
-  public @ResponseBody ResponseEntity<String> example() {
-    List<String> list = new ArrayList<>();
-    //return a simple list of strings
-    list.add("Congratulations, your application is up and running");
-    return new ResponseEntity<String>(list.toString(), HttpStatus.OK);
-  }
-
+  @RequestMapping(value = "/stockquotesb/{stock}")
+  public ResponseEntity<?> getStockQuote(@PathVariable("stock") String stock) {
+       
+        
+        return new ResponseEntity<String>("Symbol: " + stock + " Price: " + TEST_PRICE, HttpStatus.OK);
+    }
 
 }
